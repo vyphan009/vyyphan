51be4aa5bc0eb5d1f85522057be3e1cbac70cd07
vyphan009 <vyphan.cnp@gmail.com>
2019-02-28 11:54:26 -0800

    s

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ res.setHeader('Content-Type', 'text/html');
 res.end('<h1>Hello World</h1>');
 });
 server.listen(port,() => {
-console.log(`Server running at port `+port);
+console.log(`Server running at port `+ port);
 });
