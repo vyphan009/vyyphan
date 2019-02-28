c11464eeda4ee88b27f978ca7d3169cdd0722b8c
vyphan009 <vyphan.cnp@gmail.com>
2019-02-27 19:10:11 -0800

    added

--- COMP4711/script.js
+++ COMP4711/script.js
@@ -3,19 +3,3 @@ function goBack()
 window.history.back();
 };
 
-var wrap = document.getElementById("wrapper");
-
-var block = document.createElement("img");
-    document.write("<img src=" + "../images/red.jpg" + ",style=" + "\"background-color:" + "blue;\""+ ">");
-        
-var score = 10;
-    document.write("<p>" + score + "</p>");
-
-    
-function terminate(){
-    score = 0;
-}
-function flip(){
-    
-}
-
