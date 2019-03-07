12a08f60c23ac5104bb166fa7eaae848d033ee5a
vyphan009 <vyphan.cnp@gmail.com>
2019-03-07 14:04:45 -0800

    a

--- COMP4711/MemoryGame/Model/submit.js
+++
@@ -1,7 +0,0 @@
-function submit(){
-	let name=document.getElementById("name").value;
-	window.location.href="/Users/phanvy/Documents/term4/web/vyyphan/COMP4711/MemoryGame/View/leaderboard.html";
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("GET", "http://localhost::8888/?name=" + name, true);
-	xhttp.send();
-}
